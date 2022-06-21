cfe3f264a882bceb4ccf83d70b45169bfadbabd6
Alexander28-31 <bednenko1604@gmail.com>
2022-06-21 20:01:09 +0300

    Versia_1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -51,3 +51,89 @@ class Comments(models.Model):
 
     def __str__(self):
         return self.text[:15]
+
+from django.db import models
+
+from .validators import validate_year
+
+
+class Categories(models.Model):
+    """Модель типов произведений."""
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+
+class Genres(models.Model):
+    """Модель Жанров произведений."""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ['name']
+
+
+class Titles(models.Model):
+    """Модель произведений."""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    year = models.IntegerField(
+        verbose_name='Дата выхода',
+        validators=[validate_year]
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        null=True,
+        default=None
+    )
+    genre = models.ManyToManyField(
+        Genres,
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ['name']
+
