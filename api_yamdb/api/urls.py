545f0245616dd7896ff51cf44d6fc2d8ae510352
Ho1yGun <95239268+Ho1yGun@users.noreply.github.com>
2022-06-26 18:45:15 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -37,5 +37,4 @@ urlpatterns = [
     path('v1/', include('djoser.urls.jwt')),
     path('v1/auth/', include('users.urls', namespace='users')),
     path('v1/auth/', include('django.contrib.auth.urls'))
-    path('v1/', include(router_v1.urls))
 ]
