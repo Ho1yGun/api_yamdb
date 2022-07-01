79eb95976f04a3dc9c577bf40dbc83f316f05041
Андрей Рожков <russian.new.creative@gmail.com>
2022-07-01 22:36:43 +0300

    'pep8_edits2'

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,12 +3,10 @@ from rest_framework import permissions
 
 class OnlyAdminDeleteReviewsAndComments(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.is_moderator
-                or request.user.is_staff
-        )
+                or request.user.is_staff)
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
@@ -18,8 +16,8 @@ class OnlyAdminDeleteReviewsAndComments(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and
-                    (request.user.is_admin or request.user.is_superuser)))
+                or (request.user.is_authenticated
+                    and (request.user.is_admin or request.user.is_superuser)))
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
