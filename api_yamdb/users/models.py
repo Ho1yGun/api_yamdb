4cfede770747158d6758b230bffc9d1270d62998
Ho1yGun <95239268+Ho1yGun@users.noreply.github.com>
2022-07-01 21:46:44 +0300

    Update models.py
    update users/models

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -8,13 +8,33 @@ class User(AbstractUser):
     MODERATOR = 'moderator'
     ADMIN = 'admin'
 
-    # Константы заглавными буквами ROLE_CHOISES.
-    # Название выбора в константы.
     ROLE_CHOISES = {
         (USER, 'user'),
         (MODERATOR, 'moderator'),
         (ADMIN, 'admin')
     }
+
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=150,
+        null= True,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=50,
+        blank=True
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=50,
+        blank=True
+    )
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=150,
+        unique=True
+    )
     bio = models.TextField(
         'Краткая информация',
         blank=True,
@@ -26,7 +46,6 @@ class User(AbstractUser):
         choices=ROLE_CHOISES,
         default=USER
     )
-    code = models.CharField(max_length=10,  blank=True, null= True)
 
     @property
     def is_user(self):
@@ -41,6 +60,13 @@ class User(AbstractUser):
         return self.role == self.ADMIN or self.is_staff
 
     def __str__(self):
-        # Такого поля нет у User.
-        # return self.name
         return self.username
+
+
+def generate_confirmation_code():
+    return random.randint(1000, 9999)
+
+
+class ConfirmationCode(models.Model):
+    code = models.IntegerField(),
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
